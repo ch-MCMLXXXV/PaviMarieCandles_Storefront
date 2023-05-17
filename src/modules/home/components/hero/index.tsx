35ce17228e1b74c63a7e822f4b4c5e8d8c7114ac
Courtney Harris <cjharris1985@gmail.com>
2023-05-17 11:57:19 -0500

    changed wording in hero

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -6,11 +6,10 @@ const Hero = () => {
     <div className="h-[90vh] w-full relative">
       <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center text-white small:text-left small:justify-end small:items-start small:p-32">
         <h1 className="mb-4 text-2xl-semi drop-shadow-md shadow-black">
-          Spring scents are finally here!
+          Beautiful, Scented Soy Candles
         </h1>
         <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This year, our new summer collection will shelter you from the harsh
-          elements of a world that doesn&apos;t care if you live or die.
+          Leap into spring with out lovely scents!
         </p>
         <UnderlineLink href="/store">Explore products</UnderlineLink>
       </div>
