85e54a536d9df80fbba6f0e16c347183a56e8af0
Courtney Harris <cjharris1985@gmail.com>
2023-05-17 18:04:51 -0500

    changes text of featured products

--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -8,13 +8,13 @@ const FeaturedProducts = () => {
 
   return (
     <div className="py-12">
-      <div className="content-container py-12">
-        <div className="flex flex-col items-center text-center mb-16">
-          <span className="text-base-regular text-gray-600 mb-6">
+      <div className="py-12 content-container">
+        <div className="flex flex-col items-center mb-16 text-center">
+          <span className="mb-6 text-gray-600 text-base-regular">
             Latest products
           </span>
-          <p className="text-2xl-regular text-gray-900 max-w-lg mb-4">
-            Our newest styles are here to help you look your best.
+          <p className="max-w-lg mb-4 text-gray-900 text-2xl-regular">
+            Our candles are here to help your home look its best.
           </p>
           <UnderlineLink href="/store">Explore products</UnderlineLink>
         </div>
